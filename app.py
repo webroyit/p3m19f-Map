8340f9b5edac4f36404c69999c8a987d812bd7a9
royweb <xytank@gmail.com>
2020-03-19 15:08:08 -0400

    Add marker to the map

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import folium
 # create map object
 m = folium.Map(location = [40.755571,-73.9465337], zoom_start = 12)
 
-# generate the code for the map 
+# create markers
+folium.Marker([40.8448089,-73.9106434],
+                popup = "<strong>Location One</strong>").add_to(m)
+
+# generate the code for the map on index.html
 m.save("index.html")
