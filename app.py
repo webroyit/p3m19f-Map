ae2180a6f89ba94bfaee0a23fb343a6bca07fa84
royweb <xytank@gmail.com>
2020-03-19 15:22:37 -0400

    Add tooltip to the marker

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import folium
 # create map object
 m = folium.Map(location = [40.755571,-73.9465337], zoom_start = 12)
 
+# global tooltip
+tooltip = "Click to see more"
+
 # create markers
+# tooltip for display message when hover
+# popup to display a popup
 folium.Marker([40.8448089,-73.9106434],
-                popup = "<strong>Location One</strong>").add_to(m)
+                popup = "<strong>Location One</strong>",
+                tooltip = tooltip).add_to(m)
 
 # generate the code for the map on index.html
 m.save("index.html")
