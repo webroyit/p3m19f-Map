aaf2b746b46dddcc2b53a5428e6f155c28600b93
royweb <xytank@gmail.com>
2020-03-20 12:00:30 -0400

    Change the color of the icon

--- app.py
+++ app.py
@@ -13,11 +13,17 @@ folium.Marker([40.8448089,-73.9106434],
                 popup = "<strong>Location One</strong>",
                 tooltip = tooltip).add_to(m)
 
-# folium.Icon() to change icon
+# folium.Icon(icon) to change icon
 folium.Marker([40.7292637,-74.0263432],
                 popup = "<strong>Location Two</strong>",
                 tooltip = tooltip,
                 icon = folium.Icon(icon = "cloud")).add_to(m)
+
+# folium.Icon(color) to change the color of the icon
+folium.Marker([40.7804975,-73.9391392],
+                popup = "<strong>Location Three</strong>",
+                tooltip = tooltip,
+                icon = folium.Icon(color = "green")).add_to(m)
 
 # generate the code for the map on index.html
 m.save("index.html")
