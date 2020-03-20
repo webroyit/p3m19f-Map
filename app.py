85a2cdcd8bf1a71f66043f72ce56a9393aeeab04
royweb <xytank@gmail.com>
2020-03-20 12:59:39 -0400

    Add logo and use it as custom icon

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ m = folium.Map(location = [40.755571,-73.9465337], zoom_start = 12)
 # global tooltip
 tooltip = "Click to see more"
 
+# create custom marker icon
+logoIcon = folium.features.CustomIcon("logo.png", icon_size = [50, 50])
+
 # create markers
 # tooltip for display message when hover
 # popup to display a popup
@@ -27,9 +30,15 @@ folium.Marker([40.7804975,-73.9391392],
 
 # folium.Icon(color, icon) to change the color and icon
 folium.Marker([40.7609969,-73.9504689],
-                popup = "<strong>Location Three</strong>",
+                popup = "<strong>Location Four</strong>",
                 tooltip = tooltip,
                 icon = folium.Icon(color = "red", icon = "leaf")).add_to(m)
+
+# folium.Icon(color) to change the color of the icon
+folium.Marker([40.7183356,-73.9549321],
+                popup = "<strong>Location Five</strong>",
+                tooltip = tooltip,
+                icon = logoIcon).add_to(m)
 
 # generate the code for the map on index.html
 m.save("index.html")
